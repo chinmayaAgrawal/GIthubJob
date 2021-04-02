a7aaa4804b4f37e57936a6875f6fc76d749b7c7e
Chinmaya Agrawal <chinmaya953agrawal@gmail.com>
2021-04-02 18:02:35 +0530

    raw job title is now displayable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 //import './App.css';
 
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
+import Job from './Job'
 function App() {
     
+  
+  const [params, setParams]= useState({})
+  //inital state to be for page 1
+  const [page,setPage] =useState(1)
+
   //while fetching for jobs the app too will be loading and in case there
   // may be any an error so  
   const {jobs,loading,error} = useFetchJobs();
@@ -15,7 +21,11 @@ function App() {
 
       {loading && <h1>LOADING ......</h1>}
       { error && <h1> ERROR OCCURED.... TRY AGAIN  :)</h1>}
-      <h1>{jobs.length}</h1>
+      {//<h1>{jobs.length}</h1>} //for displaying jobs as in res 
+}
+       {jobs.map(job => {
+         return <Job key={job.id} job={job}/>
+       })}
 
     </Container>
     /*
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -13,6 +13,9 @@ const ACTIONS = {
 
 const BASE_URL = ' https://corsanywhere.herokuapp.com/https://jobs.github.com/positions.json'
 //no longer supported /// https://api.allorigins.win/raw?url=/
+//facing some problem with it https://cors-anywhere.herokuapp.com
+
+
 function reducer(state, action) {
     switch (action.type) {
       case ACTIONS.MAKE_REQUEST:
@@ -29,6 +32,8 @@ function reducer(state, action) {
 
 //the function takes two input parametes parmas- for location ,type,long/lat
 //page -- for the page we are currently in
+
+
 export default function useFetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
     //payload is date for the given type
@@ -37,10 +42,16 @@ export default function useFetchJobs(params, page) {
     //whenever there is a change in either params or page we need to reload and so
     // useEffect is the react hook fot it
     
-    useEffect(() => {
+
+    // so there is a problem (small one) with the present code whenever the params change quickly
+    //the response too comes quickly so it need to be modifed with cancel token part.
+
+    const CancelToken =axios.CancelToken.source();
+  useEffect(() => {
         //const cancelToken1 = axios.CancelToken.source()
-        dispatch({ type: ACTIONS.MAKE_REQUEST })
-        axios.get(BASE_URL, {
+    dispatch({ type: ACTIONS.MAKE_REQUEST })
+    axios.get(BASE_URL, {
+            CancelToken:CancelToken.token,
             //covert the given data to markdown as specified in documentation
             params: { markdown: true, page: page, ...params },
             
@@ -49,9 +60,18 @@ export default function useFetchJobs(params, page) {
             dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } }) 
     }).catch(e => {
             //for any error that can occur
+
+            //if by default the cleaning is occuring fot that reeason
+            if(axios.isCancel(e)) return
             dispatch({ type: ACTIONS.ERROR, payload: { error: e } }) 
         })
-    },[params,page])
+    
+    
+        ///function to clear all pervious search response request
+    return() =>{
+      CancelToken.cancel()
+    }  
+  },[params,page])
 
     return state
 
